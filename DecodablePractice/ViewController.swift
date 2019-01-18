50aa6ae11bae6ae4d0de49cbaefc697e18a19276
Stephen Dowless <stephendowless@homesnapadmins-macbook-pro-2.local>
2019-01-17 22:53:10 -0500

    Initial Commit

---
+++ DecodablePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DecodablePractice
+//
+//  Created by Stephen Dowless on 1/17/19.
+//  Copyright © 2019 Stephan Dowless. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
